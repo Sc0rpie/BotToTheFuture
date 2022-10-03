0ea624553eaa61bfad93e4570f36d3fdfa2a2adb
Sc0rpie <olen2003@gmail.com>
2022-10-03 23:09:27 +0300

    Added team name rand, fixed roles, etc.
    Roles are now being assigned properly to both the channel and the user.
    New comments were added.
    Channel permissions are now properly assigned.
    Bot starts hanging/not working from time to time.
    There's still some unfinished code in regards to channel/role creation

--- bot.py
+++ bot.py
@@ -1,27 +1,28 @@
-# This example requires the 'members' and 'message_content' privileged intents to function.
-
 from glob import glob
 from http import client
+from logging import RootLogger
 from re import A
 from venv import create
 import discord
 from discord.utils import *
 from discord.ext import commands
 import random
+from functions import *
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-
-There are a number of utility commands being showcased here.'''
+description = '''A special bot made for a special event called "CatToTheFuture"'''
 
 a = 0
 teamNum = 1
 
-intents = discord.Intents.default()
+intents = discord.Intents.default() # Intent - ability of a bot to do something (example: message intent for messaging and etc.)
 intents.members = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', description=description, intents=intents)
+bot = commands.Bot(command_prefix='$', description=description, intents=intents) # Bot commands start with a prefix (here it's $)
+
+async def channelCreate(): # For code to look cleaner (not implemented yet)
+    pass
+
 
 @bot.event
 async def on_ready():
@@ -30,12 +31,11 @@ async def on_ready():
 
 @bot.command()
 @commands.cooldown(1, 10800, commands.BucketType.user)
-async def start(ctx):
-    global a
-    global teamNum
-    #role = discord.utils.get(ctx.guild.roles, name="Admin")
+async def start(ctx): # Let the chaos begin. ($start command for members to join a team)
+    global a # Team members
+    global teamNum # Team number
 
-    if ctx.channel.id != 1026012141867769876:
+    if ctx.channel.id != 1026528350891679754: # Check if text channel is #bot. If not, error is thrown
         await ctx.send("PaChDoN huuh??")
         with open('huuh.png', 'rb') as fp:
             await ctx.send(file=discord.File(fp, 'huuh.png'))       
@@ -43,27 +43,63 @@ async def start(ctx):
     elif a == 0:
         guild = ctx.guild
         user = ctx.message.author
-        #team_role = get(guild.roles, name="cumpiss" + str(teamNum))
-        channel = await guild.create_text_channel("cumpiss " + str(teamNum))
-        await channel.set_permissions(ctx.guild.default_role, view_channel=False)
-        await guild.create_role(name="cumpiss " + str(teamNum)) #padaro roles
-        role = discord.utils.get(user.guild.roles, name = "cumpiss " + str(teamNum))
+        teamName = randomTeamName()
+        await guild.create_role(name = str(teamName))
+        global role
+        role = discord.utils.get(user.guild.roles, name = str(teamName))
+        overwrites = discord.PermissionOverwrite()
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(view_channel = False),
+            role: discord.PermissionOverwrite(view_channel = True)
+        }
+
+        await guild.create_text_channel(str(teamName), overwrites = overwrites)
         a += 1
-    elif a == 2:
-        teamNum += 1
+        await ctx.message.delete() # Delete message to have a clean main channel
+    elif a == 2: 
         guild = ctx.guild
-        channel = await guild.create_text_channel("cumpiss " + str(teamNum))
-        await channel.set_permissions(ctx.guild.default_role, view_channel=False)
-        await guild.create_role(name="cumpiss " + str(teamNum)) 
         user = ctx.message.author
-        role = discord.utils.get(user.guild.roles, name = "cumpiss " + str(teamNum))
+        teamName = randomTeamName()
+        role = discord.utils.get(user.guild.roles, name = str(teamName))
+        overwrites = discord.PermissionOverwrite()
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(view_channel = False),
+            role: discord.PermissionOverwrite(view_channel = True)
+        }
+
+        await guild.create_text_channel(str(teamName), overwrites = overwrites)
         a = 1
+        await ctx.message.delete() # Delete message to have a clean main channel
     else:
         user = ctx.message.author
-        role = discord.utils.get(user.guild.roles, name = "cumpiss " + str(teamNum))
         a += 1
+        await ctx.message.delete() # Delete message to have a clean main channel
     await user.add_roles(role)
-  
+
+
+
+@bot.command()
+# @commands.cooldown(1, 10800, commands.BucketType.user)
+async def test(ctx): # Team text channel and role creation test (each time $test is run, new role and channel is created)
+    global a # Team members
+    global teamNum # Team number
+
+    if a == 0:
+        guild = ctx.guild
+        global user
+        user = ctx.message.author
+        teamName = randomTeamName()
+        print(teamName)
+        await guild.create_role(name = str(teamName))
+        role = discord.utils.get(user.guild.roles, name = str(teamName))
+        overwrites = discord.PermissionOverwrite()
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(view_channel = False),
+            role: discord.PermissionOverwrite(view_channel = True)
+        }
+        await guild.create_text_channel(str(teamName), overwrites = overwrites)
+    await user.add_roles(role)
+
 @start.error
 async def helloworld_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
--- functions.py
+++ functions.py
@@ -1,8 +1,20 @@
+import random
+
+teamNames = ["Nyan", "Yoshi", "Haruki", "Kenzo", "Momo", "Yuki",
+            "Fuwafuwa", "Tadeo", "Yuuma", "Adzuki", "Taro", "Sota", "Fuku"]
+
+i = 0
+
 def fileRead():
     with open('testingFiles.txt') as myFile:
         text = myFile.read()
     result = text.split('$')
     return result
 
-def startRoles():
-    pass
+def randomTeamName():
+    # global i
+    # name = teamNames[i]
+    # i += 1
+    name = random.choice(teamNames)
+    teamNames.remove(name)
+    return name
